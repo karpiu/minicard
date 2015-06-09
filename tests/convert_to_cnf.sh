f67743f35c3784e0a5cfbbba4c6c5b381942c7a4
Michal Karpinski <karp0506@gmail.com>
2015-06-09 17:21:16 +0200

    cnfp to cnf converting script added

---
+++ tests/convert_to_cnf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+output_dir="converted/"
+
+mkdir $output_dir
+
+for file in $(find msu4-suite -type f -name "*.cnfp.gz"); do
+
+    dir_path=$output_dir$(dirname $file)
+
+    if [ ! -e $dir_path ]; then
+	echo "Creating $dir_path"
+	mkdir -p $dir_path
+    fi
+
+    echo "Processing "$(basename $file)
+    
+    file_path=$output_dir$file".cnf"
+
+    ../minicard_encodings/minicard_encodings_static -warn=0 -verb=3 -encode-type=8 -convert-to=1 $file > $file_path
+    gzip $file_path
+done
