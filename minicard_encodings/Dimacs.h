3058417fa779447aa70d3e3dee3751b22d670e83
Michal Karpinski <karp0506@gmail.com>
2016-01-25 11:21:11 +0100

    small changes in minicard_encodings folder

--- minicard_encodings/Dimacs.h
+++ minicard_encodings/Dimacs.h
@@ -120,7 +120,7 @@ static void parse_DIMACS_main(B& in, Solver& S) {
 template<class Solver>
 static void parse_DIMACS(gzFile input_stream, Solver& S) {
     StreamBuffer in(input_stream);
-    parse_DIMACS_main(in, S); }
+    parse_DIMACS_main(in, S); S.encoder.writeMaps(); }
 
 //=================================================================================================
 }
